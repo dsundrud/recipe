251b4cbc5425db0eec434bf28164c6d6da043a88
David Sundrud <dsundrud@gmail.com>
2022-08-28 18:08:31 -0700

    Initial Commit

---
+++ recipe/recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeApp.swift
+//  recipe
+//
+//  Created by David Sundrud on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
